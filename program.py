c58961af06f433eadb0570ebfd5a42166a40a49b
Ana Finotelli <finotelli2004@gmail.com>
2023-09-06 15:30:21 -0300

    if.python

---
+++ program.py
@@ -0,0 +1,9 @@
+# neste programa estou aprendendo como aplicar o if e else
+a = int(input('Digite um valor numérico: '))
+if (a < 10) :
+    print('Você digitou um número menor que 10.')
+print('FIM DO PROGRAMA')
+
+# no código acima, se o número que a pessoa digitar no terminal for menor que 10, resultará na mensagem "voce digitou um numero menor que 10"
+# e finalizará finalizando o programa com a mensagem avisando.
+# já se o numero digitado for maior que 10, o programa não exibirá nenhuma mensagem e apenas será finalizado.
