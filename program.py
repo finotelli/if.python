1033ffdf9e58110e8420ca099af11049f29672d2
Ana Finotelli <finotelli2004@gmail.com>
2023-09-11 13:16:43 -0300

    if.python

--- program.py
+++ program.py
@@ -2,8 +2,11 @@
 a = int(input('Digite um valor numérico: '))
 if (a < 10) :
     print('Você digitou um número menor que 10.')
+else :
+    print('Você digitou um número maior ou igual a 10. ')
 print('FIM DO PROGRAMA')
 
 # no código acima, se o número que a pessoa digitar no terminal for menor que 10, resultará na mensagem "voce digitou um numero menor que 10"
 # e finalizará finalizando o programa com a mensagem avisando.
-# já se o numero digitado for maior que 10, o programa não exibirá nenhuma mensagem e apenas será finalizado.
+# já se o numero digitado for maior/igual a 10, o programa exibirá uma mensagem avisando que o numero é igual/maior que 10.
+# por fim, finalizará o programa.
